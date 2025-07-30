d191580a48a1edd2a569d5ada894d2b992ac9347
Leejun <estherleejunkim@gmail.com>
2025-07-30 10:04:50 +0900

    수정 OpenApiResponseDto

--- src/main/java/com/codeit/findex/dto/OpenApiResponseDto.java
+++ src/main/java/com/codeit/findex/dto/OpenApiResponseDto.java
@@ -2,57 +2,35 @@ package com.codeit.findex.dto;
 
 import java.util.List;
 
-public record OpenApiResponseDto(
-    HeadAndBodyDto response
-) {
+public record OpenApiResponseDto(HeadAndBodyDto response) {
+  public record HeadAndBodyDto(HeaderDto header, BodyDto body) {}
 
+  public record HeaderDto(String resultCode, String resultMsg) {}
+
+  public record BodyDto(Integer numOfRows, Integer pageNo, Integer totalCount, ItemsDto items) {}
+
+  public record ItemsDto(List<IndexItemDto> item) {}
+
+  public record IndexItemDto(
+      String basPntm,
+      Double basIdx,
+      String basDt,
+      String idxCsf,
+      String idxNm,
+      Integer epyItmsCnt,
+      Double clpr,
+      Double vs,
+      Double fltRt,
+      Double mkp,
+      Double hipr,
+      Double lopr,
+      Long trqu,
+      Long trPrc,
+      Long lstgMrktTotAmt,
+      Double lsYrEdVsFltRg,
+      Double lsYrEdVsFltRt,
+      Double yrWRcrdHgst,
+      String yrWRcrdHgstDt,
+      Double yrWRcrdLwst,
+      String yrWRcrdLwstDt) {}
 }
-
-record HeadAndBodyDto (
-    HeaderDto header,
-    BodyDto body
-) {}
-
-record BodyDto(
-    Integer numOfRows,
-    Integer pageNo,
-    Integer totalCount,
-    ItemsDto items
-) {
-
-}
-
-record ItemsDto (
-    List<IndexItemDto> item
-) {}
-
-record HeaderDto(
-    String resultCode,
-    String resultMsg
-) {
-}
-
-record IndexItemDto(
-    String basPntm,
-    Double basIdx,
-    String basDt,
-    String idxCsf,
-    String idxNm,
-    Integer epyItmsCnt,
-    Double clpr,
-    Double vs,
-    Double fltRt,
-    Double mkp,
-    Double hipr,
-    Double lopr,
-    Long trqu,
-    Long trPrc,
-    Long lstgMrktTotAmt,
-    Double lsYrEdVsFltRg,
-    Double lsYrEdVsFltRt,
-    Double yrWRcrdHgst,
-    String yrWRcrdHgstDt,
-    Double yrWRcrdLwst,
-    String yrWRcrdLwstDt
-) {}
-
