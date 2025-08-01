d78e28582fdc96c44b8cc133dcc0c96c753a3583
hochan <106953872+cheis11@users.noreply.github.com>
2025-08-01 15:30:01 +0900

    추가 GlobalExceptionHandler

--- src/main/java/com/codeit/findex/mapper/IntegrationMapper.java
+++ src/main/java/com/codeit/findex/mapper/IntegrationMapper.java
@@ -15,7 +15,6 @@ public interface IntegrationMapper {
   @Mapping(expression = "java(com.codeit.findex.entityEnum.JobType.INDEX_INFO)", target = "jobType")
   @Mapping(target = "indexInfoId", source = "indexInfo.id")
   @Mapping(target = "targetDate", source = "indexInfo.basepointInTime")
-  @Mapping(target = "worker", source = "worker")
   @Mapping(expression = "java(java.time.LocalDateTime.now())", target = "jobTime")
   @Mapping(expression = "java(com.codeit.findex.entityEnum.Result.SUCCESS)", target = "result")
   SyncJobDto toSyncJobDto(long integrationId, IndexInfo indexInfo, String worker);
@@ -24,7 +23,6 @@ public interface IntegrationMapper {
   @Mapping(expression = "java(com.codeit.findex.entityEnum.JobType.INDEX_DATA)", target = "jobType")
   @Mapping(target = "indexInfoId", source = "indexData.indexInfo.id")
   @Mapping(target = "targetDate", source = "indexData.baseDate")
-  @Mapping(target = "worker", source = "worker")
   @Mapping(expression = "java(java.time.LocalDateTime.now())", target = "jobTime")
   @Mapping(expression = "java(com.codeit.findex.entityEnum.Result.SUCCESS)", target = "result")
   SyncJobDto toSyncJobDto(long integrationId, IndexData indexData, String worker);
