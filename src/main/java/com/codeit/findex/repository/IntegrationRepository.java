bdb94661857671e645ac16409d45846ae3572a4e
문은서 <meunse1999@naver.com>
2025-08-08 00:05:40 +0900

    수정 deleteIndexInfo

--- src/main/java/com/codeit/findex/repository/IntegrationRepository.java
+++ src/main/java/com/codeit/findex/repository/IntegrationRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -43,6 +44,8 @@ WHERE i.indexInfo.id = :indexInfoId
       @Param("worker") String worker,
       @Param("afterTime") LocalDateTime afterTime);
 
-  @Transactional
-  void deleteAllByIndexInfoId(Long indexInfoId);
+  @Modifying
+  @Query("DELETE FROM Integration i WHERE i.indexInfo.id = :indexInfoId")
+  void deleteAllByIndexInfoId(@Param("indexInfoId") Long indexInfoId);
 }
+
